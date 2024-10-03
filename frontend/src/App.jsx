ecb785fd8df2f38ac6fb86b7d223e6b9c593f796
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-10-03 15:30:44 +0300

    init react js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import styles from './App.module.css';
+import {en, Faker} from '@faker-js/faker';
+
+export const faker = new Faker({
+  locale: [en],
+});
+
+const App = () => {
+  const [posts, setPosts] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const addPost = () => {
+    const randomName = faker.internet.userName();
+    const uniqueId = faker.string.uuid(); // Generate a unique ID for each post
+
+    if (posts.trim()) {
+      setMessages([...messages, {id: uniqueId, name: randomName, message: posts}]);
+      setPosts(''); // Clear the input after adding the post
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <p>Chat</p>
+      <div className={styles.board}>
+        <div className={styles.messages}>
+          {messages.map((post) => (
+            <p key={post.id}>
+              <span>😁{post.name}</span> - {new Date().toLocaleString()}
+              <br/>
+              {post.message}
+            </p>
+          ))}
+        </div>
+        <div className={styles.input}>
+          <textarea
+            value={posts}
+            onChange={(e) => setPosts(e.target.value)}
+            placeholder="Enter your message"
+            rows="4"
+            cols="50"
+          />
+          <button onClick={addPost}>Add post</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
